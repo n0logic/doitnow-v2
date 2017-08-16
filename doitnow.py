70c679eef573226aa1d4b0f3ae65b27d8520a3ea
n0logic <n0logic@n0tlogical>
2017-08-16 16:05:48 -0400

    testing123

---
+++ doitnow.py
@@ -0,0 +1,17 @@
+#!/bin/python
+#
+# doitnow.py
+# Do It NOW!!! v2
+# New and improved "helper" script aka Mah Lazy Kali SCRIPT
+# Author - n0logic
+# Version - v2
+# Date Created - 8/16/2017
+# Last Modified - 8/16/2017
+# on github @ https://github.com/n0logic/doitnow-v2/
+# Reach me via Twitterzz @realn0logic
+# or Emailzz via daniel@ [insert n0logic here- bot avoidance].com
+# Good day sir/madam!
+#
+# Let do this shizz
+#
+
