8dd4d8d7db37ac829c2c99a7e5a7f007196c2dc3
n0logic <daniel@n0logic.com>
2017-08-28 11:18:18 -0700

    Update doitnow.py

--- doitnow.py
+++ doitnow.py
@@ -2,7 +2,7 @@
 #
 # doitnow.py
 # Do It NOW!!! v2
-# New and improved "helper" script aka Mah Lazy Kali SCRIPT
+# New and improved *maybe*
 # Author - n0logic / besgood
 # Version - v2
 # Date Created - 8/16/2017
